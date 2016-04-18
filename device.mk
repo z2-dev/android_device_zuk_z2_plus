36cb03953361ce1e132ad5fa4eb64d6fcb35263e
Bruno Martins <bgcngm@gmail.com>
2016-04-18 16:33:35 +0100

    z2_plus: Add GPS configs
    Change-Id: I1727f746d30c7d1d7d22628117ebf28b148322ae

--- device.mk
+++ device.mk
@@ -176,13 +176,12 @@ PRODUCT_PACKAGES += \
     gps.msm8996 \
     libcurl
 
+# GPS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # Graphics
 PRODUCT_PACKAGES += \
