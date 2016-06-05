82fbdb73c29889792b5003378a8014d1fddf45a5
Bruno Martins <bgcngm@gmail.com>
2016-06-05 16:16:13 +0100

    z2_plus: Build GPS HAL from source
    * Remove Location API for targets with RPC
    * Use stock GPS configs (also include missing xtwifi.conf)
    
    Change-Id: I142d161b324e1bc84d258adbedb0a454a72e7e4c

--- device.mk
+++ device.mk
@@ -173,15 +173,14 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    gps.msm8996 \
-    libcurl
+    gps.msm8996
 
-# GPS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
 # Graphics
 PRODUCT_PACKAGES += \
