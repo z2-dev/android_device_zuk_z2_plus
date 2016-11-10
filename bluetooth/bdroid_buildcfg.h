521f845e7af3d669a2826fdbc459ef3e14c2fb79
Fedor917 <cryscript@gmail.com>
2016-11-10 15:54:27 +0700

    Change Bluetooth device name for ZUK Z2 Plus

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,7 +21,7 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "Zuk Z2"
+#define BTM_DEF_LOCAL_NAME   "ZUK Z2 Plus"
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_ACL_CONNECTIONS    7
