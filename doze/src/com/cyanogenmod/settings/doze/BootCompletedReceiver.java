85605b1c07bdaefcb735bbbcb4ca73ba23c970b5
Fedor917 <cryscript@gmail.com>
2016-11-10 16:00:35 +0700

    Rename CmDoze to ZukDoze

--- doze/src/com/cyanogenmod/settings/doze/BootCompletedReceiver.java
+++ doze/src/com/cyanogenmod/settings/doze/BootCompletedReceiver.java
@@ -24,7 +24,7 @@ import android.util.Log;
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
-    private static final String TAG = "OneplusDoze";
+    private static final String TAG = "ZukDoze";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
