51fe6eb1975b0978ebe8eaa9cf8a7e4d01d2525b
davidevinavil <davidevinavil@gmail.com>
2017-01-10 01:17:12 +0100

    add key overlay thanks to Fedor
    Change-Id: I88ad7e899b36d4660b116aaa1e5b8a13a1dcfb1b

--- device.mk
+++ device.mk
@@ -260,7 +260,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/hostapd.accept:system/etc/hostapd/hostapd.accept \
-    $(LOCAL_PATH)/wifi/hostapd.conf:system/etc/hostapd/hostapd_default.conf \
+    $(LOCAL_PATH)/wifi/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
     $(LOCAL_PATH)/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
