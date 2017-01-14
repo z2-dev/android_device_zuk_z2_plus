5e20df24d5e2d84140e48760f617b0f321b124e4
davidevinavil <davidevinavil@gmail.com>
2017-01-14 16:22:38 +0100

    Build ifaa
    Change-Id: I20856c327ea721f10a41f3a1afe888717f0747cf

--- device.mk
+++ device.mk
@@ -26,6 +26,14 @@ $(call inherit-product-if-exists, vendor/zuk/z2_plus/z2_plus-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Alipay
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
