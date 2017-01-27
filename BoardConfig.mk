383eabacae4e4e74d32fd415c26db134964b2fb3
davidevinavil <davidevinavil@gmail.com>
2017-01-27 19:49:29 +0100

    Rootdir: fix build
    Change-Id: I9d2e496b3b91d171587909a27117e38ebe61b007

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,7 +175,7 @@ TARGET_RIL_VARIANT := caf
 SIM_COUNT := 2
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/fstab.qcom
 TARGET_RECOVERY_UI_LIB := librecovery_ui_msm
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
 TARGET_RELEASETOOLS_EXTENSIONS := device/qcom/common
