3459a1b8cb2569c811a0c698542f2f7ac76b9cbd
ontherunvaro <alvaro.brv@gmail.com>
2017-02-11 12:09:24 +0100

    Set virtual keys as virtual (duh), and dont do haptic feedback by hand

--- keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
+++ keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -16,10 +16,7 @@
 
 package com.cyanogenmod.settings.device;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Handler;
 import android.os.Vibrator;
 import android.view.KeyEvent;
 
@@ -58,47 +55,36 @@ public class KeyHandler implements DeviceKeyHandler {
 
         switch (scanCode) {
             case PRESS_SCANCODE:
-                if(event.getAction() == KeyEvent.ACTION_DOWN){
-		  mPressDown=true;
-		  android.util.Log.d(TAG, "handleKeyEvent: mPressDown set to true");
-                }else{
-		  mPressDown=false;
-  		  android.util.Log.d(TAG, "handleKeyEvent: mPressDown set to false");
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    mPressDown = true;
+                    android.util.Log.d(TAG, "handleKeyEvent: mPressDown set to true");
+                } else {
+                    mPressDown = false;
+                    android.util.Log.d(TAG, "handleKeyEvent: mPressDown set to false");
                 }
                 break;
             case TAP_SCANCODE:
                 // nothing, just let it through
                 break;
             case LONG_TAP_SCANCODE:
-		if(mPressDown){
-		  android.util.Log.d(TAG, "handleKeyEvent: mPressDown is true, LONG_TAP ignored");
-		  return true;
+                if (mPressDown) {
+                    android.util.Log.d(TAG, "handleKeyEvent: mPressDown is true, LONG_TAP ignored");
+                    return true;
                 }
                 break;
             case SWIPE_RIGHT_SCANCODE:
-                doHapticFeedback();
+                // nothing, just let it through
                 break;
             case SWIPE_LEFT_SCANCODE:
-                doHapticFeedback();
+                // nothing, just let it through
                 break;
             default:
                 return false;
         }
 
-        
+
         return false;
 
     }
 
-
-    private void doHapticFeedback() {
-        if (mVibrator == null) {
-            return;
-        }
-        boolean enabled = CMSettings.System.getInt(mContext.getContentResolver(),
-                CMSettings.System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK, 1) != 0;
-        if (enabled) {
-            mVibrator.vibrate(20);
-        }
-    }
 }
