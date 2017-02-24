52e1983e8aee7ee85570de3816e7e9ed71f2c8b4
Álvaro Brey <alvaro.brv@gmail.com>
2017-02-25 00:42:25 +0100

    Use Gello for browser

--- device.mk
+++ device.mk
@@ -132,6 +132,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
+# Browser
+PRODUCT_PACKAGES += \
+		Gello
+
+
 # Camera
 PRODUCT_PACKAGES += \
     Camera2
