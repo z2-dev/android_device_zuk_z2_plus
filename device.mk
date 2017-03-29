54e6b7919625f361cdea603dce57ff02f689e25a
LuK1337 <priv.luk@gmail.com>
2017-03-29 23:22:13 +0200

    z2_plus: Move ro.product.first_api_level to system.prop
    Change-Id: I831c553f1b941f3e5e56369e72420180e1f34138

--- device.mk
+++ device.mk
@@ -26,10 +26,6 @@ $(call inherit-product-if-exists, vendor/zuk/z2_plus/z2_plus-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Device was launched with M
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product.first_api_level=23
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
