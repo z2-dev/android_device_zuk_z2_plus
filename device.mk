b794e7641a4802878089b3ad3f4a3997b323b0ca
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2017-09-19 17:52:06 +0200

    Decommonize ro.sf.lcd_density and ro.qualcomm.cabl props.

--- device.mk
+++ device.mk
@@ -28,3 +28,10 @@ $(call inherit-product, device/zuk/msm8996-common/msm8996.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml
+
+# System properties
+PRODUCT_PROPERTY_OVERRIDES += \
+# Graphics
+    ro.sf.lcd_density=480 \
+# Display (Qualcomm AD)
+    ro.qualcomm.cabl=2
